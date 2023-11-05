7da71a41d37bb35cc73a324800c0896fb1d19e99
Guy <guyelizarov@gmail.com>
2023-11-05 12:59:37 +0200

    adding filtering  by success

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,55 +1,72 @@
-import { Center, Button, Spinner, Flex, VStack } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { landingService } from '../services/landingService.js';
-import Card from '../cmps/card.jsx';
+import { Center, Button, Spinner, Flex, VStack, Select, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { landingService } from '../services/landingService.js'
+import Card from '../cmps/card.jsx'
 
 export const Home = () => {
-    const [allLandings, setAllLandings] = useState([]);
-    const [landings, setLandings] = useState([]);
-    const [page, setPage] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const limit = 20; // Number of items per page
+    const [allLandings, setAllLandings] = useState([])
+    const [landings, setLandings] = useState([])
+    const [page, setPage] = useState(0)
+    const [loading, setLoading] = useState(false)
+    const [filter, setFilter] = useState('all') // Added filter state
+    const limit = 20
 
     useEffect(() => {
-        loadAllLandings();
-    }, []);
+        loadAllLandings()
+    }, [])
 
     useEffect(() => {
-        // Compute and set the landings for the current page
-        const start = page * limit;
-        const end = start + limit;
-        setLandings(allLandings.slice(start, end));
-    }, [page, allLandings]);
+        const filteredLandings = allLandings.filter(landing => {
+            if (filter === 'all') return true
+            return filter === 'success' ? landing.success : !landing.success
+        })
+
+        const start = page * limit
+        const end = start + limit
+        setLandings(filteredLandings.slice(start, end))
+    }, [page, allLandings, filter])
 
     const loadAllLandings = async () => {
-        setLoading(true); // Set loading to true while fetching data
+        setLoading(true)
         try {
-            const data = await landingService.getLandings();
-            setAllLandings(data); // Store all landings
-            setLandings(data.slice(0, limit)); // Set initial page
+            const data = await landingService.getLandings()
+            setAllLandings(data)
+            setLandings(data.slice(0, limit))
         } catch (error) {
-            console.error('Error loading landings:', error);
+            console.error('Error loading landings:', error)
         } finally {
-            setLoading(false); // Set loading to false after fetching data
+            setLoading(false)
         }
-    };
+    }
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+        setPage(0) // Reset to the first page
+    }
 
     const handleNextPage = () => {
-        const nextPage = page + 1;
-        const start = nextPage * limit;
-        if (start < allLandings.length) {
-            setPage(nextPage);
-        }
-    };
+        const nextPage = page + 1
+        setPage(nextPage)
+    }
 
     const handlePrevPage = () => {
-        setPage(page > 0 ? page - 1 : 0);
-    };
+        setPage(page > 0 ? page - 1 : 0)
+    }
 
     if (loading) return <Spinner thickness='4px' m='5' speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl' />
 
     return (
         <VStack>
+            <VStack fontSize={'24px'}  m={4} w={'50%'} h={'50px'}>
+
+                <Text>Filter by success status</Text>
+                <Select onChange={handleFilterChange}
+                     size="lg" variant='filled' >
+                    <option value="all">All Landings</option>
+                    <option value="success">Successful Landings</option>
+                    <option value="failed">Failed Landings</option>
+                </Select>
+            </VStack>
 
             <Center flexWrap={'wrap'}>
                 {landings.map(landing => (
@@ -65,8 +82,8 @@ export const Home = () => {
             </Center>
             <Flex direction="row" justify="center" align="center" m={8} gap={'5'}>
                 <Button onClick={handlePrevPage} disabled={page === 0}>Previous</Button>
-                <Button onClick={handleNextPage} disabled={page * limit + limit >= allLandings.length}>Next</Button>
+                <Button onClick={handleNextPage} disabled={(page + 1) * limit >= allLandings.filter(l => filter === 'all' || (filter === 'success' ? l.success : !l.success)).length}>Next</Button>
             </Flex>
         </VStack>
-    );
-};
+    )
+}
